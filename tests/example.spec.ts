aa51c956b6ef4ccf08f6f770e4ee4ebbdc30dcc3
olivaalbert1 <albertolivaroura@Alberts-MacBook-Air.local>
2023-10-13 14:57:54 +0200

    add test for incrementar button

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,10 +1,26 @@
 import { test, expect } from '@playwright/test'
 
-test('has title', async ({ page }) => {
+test('The counter starts at 0', async ({ page }) => {
   await page.goto('http://localhost:3000/')
 
   const counter = page.getByTestId('contador')
 
   // Expect a initial counter state "to contain" a 0.
   expect(await counter.innerText()).toBe('0')
+});
+
+test('Incrementar button adds 1 to the counter', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  const counterNumberAfterIncrement = parseInt(await page.getByTestId('contador').innerText())
+
+  // Click the incrementar button.
+  await page.getByRole('button', { name: 'Incrementar' }).click()
+
+  const counterNumberBeforeIncrement = parseInt(await page.getByTestId('contador').innerText())
+
+  const expectedResult = counterNumberAfterIncrement + 1;
+
+  // Expects an increment of 1.
+  await expect(counterNumberBeforeIncrement).toStrictEqual(expectedResult)
 });
