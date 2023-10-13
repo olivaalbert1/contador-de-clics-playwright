3e5f522d3da291b6d72e41c92effc7fdf78bd7e1
olivaalbert1 <albertolivaroura@Alberts-MacBook-Air.local>
2023-10-13 15:04:12 +0200

    add test for decrementar button

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -25,3 +25,19 @@ test('Incrementar button adds 1 to the counter', async ({ page }) => {
   expect(counterNumberBeforeIncrement).toStrictEqual(expectedResult)
 });
 
+test('Decrementar button decreases 1 to the counter', async ({ page }) => {
+  await page.goto('http://localhost:3000/')
+
+  const counterNumberAfterDecrement = parseInt(await page.getByTestId('contador').innerText())
+
+  // Click the incrementar button.
+  await page.getByRole('button', { name: 'Decrementar' }).click()
+
+  const counterNumberBeforeDecrement = parseInt(await page.getByTestId('contador').innerText())
+
+  const expectedResult = counterNumberAfterDecrement - 1;
+
+  // Expects an increment of 1.
+  expect(counterNumberBeforeDecrement).toStrictEqual(expectedResult)
+});
+
